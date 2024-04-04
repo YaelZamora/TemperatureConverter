e13e52170c93b0ef98318ca2c1d1f8bb288493b8
YaelZamora <ajedrezkasparov@gmail.com>
2024-04-04 14:37:15 -0600

    Initial Commit

---
+++ TemperatureConverter/TemperatureConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverterApp.swift
+//  TemperatureConverter
+//
+//  Created by Yael Javier Zamora Moreno on 04/04/24.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
